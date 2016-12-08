4c477aa972aa622707666520736716f61453c451
gittyRavi <rairavisaxena@gmail.com>
2016-12-08 23:50:57 +0530

    Update jquery.buttons.js

--- jquery.buttons.js
+++ jquery.buttons.js
@@ -1,26 +1,26 @@
 $(document).ready(function(){               /* for nav bar navigation */
 	$("#home").click(function(){              /* for home tab */
-		windows.location = "add url";
+		window.location = "add url";
 		return false;
 	});
 
 	$("#id1").click(function(){               /* for menu tab 1 */
-		windows.location = "add url";
+		window.location = "add url";
 		return false;
 	});
 	
 	$("#id2").click(function(){               /* for menu tab 2 */
-		windows.location = "add url";
+		window.location = "add url";
 		return false;
 	});
 	
 	$("#id3").click(function(){               /* for menu tab 3 */
-		windows.location = "add url";
+		window.location = "add url";
 		return false;
 	});
 	
 	$("#id4").click(function(){               /* for menu tab 4 */
-		windows.location = "add url";
+		window.location = "add url";
 		return false;
 	});
 });
