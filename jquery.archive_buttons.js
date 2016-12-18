b502dfaedab4d8c862a4be893f86f73d6ad8bbdb
gittyRavi <rairavisaxena@gmail.com>
2016-12-18 23:29:09 +0530

    Create jquery.archive_buttons.js

---
+++ jquery.archive_buttons.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+ 	$("#id5").click(function(){
+ 	 $("#id5").animate({
+      		 left : '85%',
+      		 opacity :'0',
+      		},2000);
+
+
+  });
+
+ 	$("#id6").click(function(){
+ 	 $("#id6").animate({
+      		 left : '85%',
+      		 opacity :'0',
+      		},2000);
+
+
+  });
+
+ 	$("#id7").click(function(){
+ 	 $("#id7").animate({
+      		 left : '85%',
+      		 opacity :'0',
+      		},2000);
+
+
+  });
+
+ 	$("#id8").click(function(){
+ 	 $("#id8").animate({
+      		 left : '85%',
+      		 opacity :'0',
+      		},2000);
+
+
+  });
+ });
