b6f4c3f5344363042cc40072cb80775b3466c79b
রবি সাক্সেনা /  Рави Саксена <gittyRavi@users.noreply.github.com>
2017-03-07 02:19:50 +0530

    Create jquery-ui_accordion.js

---
+++ coool/jquery-ui/jquery-ui_accordion.js
@@ -0,0 +1,7 @@
+/*
+<link rel='stylesheet' type='text/css' href='http://code.jquery.com/ui/1.9.1/themes/base/jquery-ui.css'/>
+        <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js"></script> <!--add this in <head> of html-->
+*/
+  $(document).ready(function(){
+     $("#menu").accordion({collapsible:true,active:false})
+  });
